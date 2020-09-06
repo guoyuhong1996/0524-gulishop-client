46832449a58207b4302d522e3973f72e4500e1a6
xiao hong <3099089368@qq.com>
2020-09-06 22:56:15 +0800

    'start'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from '@/App'
+import router from '@/router'
 
 Vue.config.productionTip = false
 
 new Vue({
+  el: 'App',
   render: h => h(App),
-}).$mount('#app')
+  // 注册路由
+  router
+})
